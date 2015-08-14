468e87dd5cc2cad569c0a4b59d0d3f4a91b14608
Siddhartha Biswas <siddhartha100@gmail.com>
2015-08-14 16:09:18 -0500

    TestProgram added

--- test/TestProgram.cpp
+++ test/TestProgram.cpp
@@ -3,28 +3,89 @@
 #include<stdio.h>
 
 int main(){
-
-// Variable declaration
-double write[1000][1000][1000];
-double read[1000][1000][1000];
-int i,j,k;
-
-while(1){
-	for(i=0;i<1000;i++){
-		for(j=0;j<1000;j++){
-			for(k=0;k<1000;k++){
-				write[i][j][k] = (i*0.01)+(j*0.01)+(k*0.01);
-			}
-		}
-	}
-	for(i=0;i<1000;i++){
-		for(j=0;j<1000;j++){
-			for(k=0;k<1000;k++){
-				read[i][j][k] = write[i][j][k];
-			}
-		}
-	}
+	printf("We are in function main now. \n");
+	a();
+	return 0;
 }
 
-return 0;
+void a(){
+	// Variable declaration
+	double write[1000][1000][1000];
+	double read[1000][1000][1000];
+	int i,j,k;
+	
+	printf("We are in function a now. \n");
+	while(1){
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					write[i][j][k] = (i*0.01)+(j*0.01)+(k*0.01);
+				}
+			}
+		}
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					read[i][j][k] = write[i][j][k];
+				}
+			}
+		}
+	}
+	b();
+}
+
+void b(){
+	// Variable declaration
+	double write[1000][1000][1000];
+	double read[1000][1000][1000];
+	int i,j,k;
+	
+	printf("We are in function b now. \n");
+	while(1){
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					write[i][j][k] = (i*0.01)+(j*0.01)+(k*0.01);
+				}
+			}
+		}
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					read[i][j][k] = write[i][j][k];
+				}
+			}
+		}
+	}
+	c();
+}
+
+void c(){
+	// Variable declaration
+	double write[1000][1000][1000];
+	double read[1000][1000][1000];
+	int i,j,k;
+	
+	printf("We are in function c now. \n");
+	while(1){
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					write[i][j][k] = (i*0.01)+(j*0.01)+(k*0.01);
+				}
+			}
+		}
+		for(i=0;i<1000;i++){
+			for(j=0;j<1000;j++){
+				for(k=0;k<1000;k++){
+					read[i][j][k] = write[i][j][k];
+				}
+			}
+		}
+	}
+	d();
+}
+
+void d(){
+	printf("We are in function d now. \n");
 }
